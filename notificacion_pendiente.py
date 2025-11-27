ad9a604b487794171597c5821ffd6e5e1b891a38
Lautaro <lautaro.gonzalez0101@gmail.com>
2025-11-27 15:07:35 -0300

    Archivos duplicados eliminados

--- notificacion_pendiente.py
+++
@@ -1,8 +0,0 @@
-from estado_notificacion import EstadoNotificacion
-
-
-class Pendiente(EstadoNotificacion):
-    """Estado: Notificación pendiente"""
-    
-    def __init__(self):
-        super().__init__("Pendiente", "Notificación pendiente de envío")
